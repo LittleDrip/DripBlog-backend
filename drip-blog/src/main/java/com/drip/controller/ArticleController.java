f2edc1a0ccef696fe81beba5d7b40f04ce1c93fc
Drip123456 <3162304527@qq.com>
2024-05-12 21:41:00 +0800

    热门文章查询实现

--- drip-blog/src/main/java/com/drip/controller/ArticleController.java
+++ drip-blog/src/main/java/com/drip/controller/ArticleController.java
@@ -1,19 +1,30 @@
 package com.drip.controller;
 
 import com.drip.service.ArticleService;
+import com.drip.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("article")
 public class ArticleController {
     @Autowired
     private ArticleService articleService;
 
-    @GetMapping("list")
-    public String test(){
-        return articleService.list().toString();
+    //    @GetMapping("list")
+//    public String test(){
+//        return articleService.list().toString();
+//    }
+    @GetMapping("hotArticleList")
+    public Result hotArticleList() {
+//        查询热门文章封装
+        Result result = articleService.hotArticleList();
+        return result;
     }
+
+
 }
