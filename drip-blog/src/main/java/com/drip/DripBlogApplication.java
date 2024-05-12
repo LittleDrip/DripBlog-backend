5a434e87aaf78a34fdc16c1f1e6ac0ab88fdc322
Drip123456 <3162304527@qq.com>
2024-05-12 21:45:34 +0800

    更改

--- drip-blog/src/main/java/com/drip/DripBlogApplication.java
+++ drip-blog/src/main/java/com/drip/DripBlogApplication.java
@@ -3,7 +3,7 @@ package com.drip;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//扫描
+//扫描mapper
 @MapperScan("com.drip.mapper")
 @SpringBootApplication
 public class DripBlogApplication {
