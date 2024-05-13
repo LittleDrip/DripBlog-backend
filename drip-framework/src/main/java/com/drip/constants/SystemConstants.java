7b91a1cea247138e4abebfe2ba7d76604c8a94d0
Drip123456 <3162304527@qq.com>
2024-05-13 17:01:52 +0800

    vo优化

---
+++ drip-framework/src/main/java/com/drip/constants/SystemConstants.java
@@ -0,0 +1,12 @@
+package com.drip.constants;
+
+public  class SystemConstants {
+    /**
+     * 草稿
+     */
+    public static final int ARTICLE_STATUS_DRAFT=1;
+    /**
+     * 正式文章
+     */
+    public static final int ARTICLE_STATUS_NORMAL=0;
+}
--- drip-framework/src/main/java/com/drip/service/impl/ArticleServiceImpl.java
+++ drip-framework/src/main/java/com/drip/service/impl/ArticleServiceImpl.java
@@ -3,14 +3,18 @@ package com.drip.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.drip.constants.SystemConstants;
 import com.drip.domain.Article;
+import com.drip.domain.vo.HotArticle;
 import com.drip.service.ArticleService;
 import com.drip.mapper.ArticleMapper;
 import com.drip.utils.Result;
 import com.drip.utils.ResultCodeEnum;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -28,12 +32,19 @@ public class ArticleServiceImpl extends ServiceImpl<ArticleMapper, Article>
     public Result hotArticleList() {
         LambdaQueryWrapper<Article> queryWrapper = new LambdaQueryWrapper<>();
 //        条件：正式文章 按照浏览量排序 最多10条(用分页实现)
-        queryWrapper.eq(Article::getStatus, 0);
+        queryWrapper.eq(Article::getStatus, SystemConstants.ARTICLE_STATUS_DRAFT);
         queryWrapper.orderByDesc(Article::getViewCount);
         Page<Article> page = new Page<>(1, 10);
         page(page, queryWrapper);
-        List<Article> records = page.getRecords();
-        return Result.ok(records);
+        List<Article> articles = page.getRecords();
+        List<HotArticle> hotArticles=new ArrayList<>();
+        for (Article article : articles) {
+//            Bean拷贝到vo
+            HotArticle vo=new HotArticle();
+            BeanUtils.copyProperties(article,vo);
+            hotArticles.add(vo);
+        }
+        return Result.ok(hotArticles);
     }
 }
 
